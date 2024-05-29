6aa190939dad128da850ab4cc2f037cf24853124
Andres Duque <andresduke024@gmail.com>
2024-05-29 16:00:04 -0500

    Initial Commit

---
+++ FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Andres Duque on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
